527b3ad0ac1aae08dd229c8234dbdcaa6a34fde8
OscarWtts <sistemasooliva@gmail.com>
2020-07-24 18:01:31 -0600

    agregando cambios

--- src/main/java/com/jes/crud/controller/PersonaRestController.java
+++ src/main/java/com/jes/crud/controller/PersonaRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,21 +16,22 @@ import com.jes.crud.model.Persona;
 import com.jes.crud.service.PersonaService;
 
 @RestController
-@RequestMapping(value = "/api") // Ruta inicial por defecto
+@RequestMapping(value = "/api") // Ruta inicial por defecto\
+@CrossOrigin("*")
 public class PersonaRestController {
 
 	@Autowired
 	PersonaService personaService;
 	
 	// *** LISTAR (READ) ****
-	// http://localhost:8888/all (GET)
+	// http://localhost:8888/api/all (GET)
 	@RequestMapping(value = "/all", method = RequestMethod.GET, produces = "application/json")
 	public List<Persona> getAll() {
 		return personaService.getAll();
 	}
 	
 	// *** CREAR Y ACTUALIZAR (CREATE AND UPDATE) ***
-	// http://localhost:8888/save (POST)
+	// http://localhost:8888/api/save (POST)
 	@RequestMapping(value = "/save", method = RequestMethod.POST, produces = "application/json")
 	public ResponseEntity<Persona> save(@RequestBody Persona persona) {
 		Persona obj = personaService.save(persona);
@@ -37,7 +39,7 @@ public class PersonaRestController {
 	}
 
 	// *** ELIMINAR (DELETE) *** 
-	// http://localhost:8888/delete/{id} (GET)
+	// http://localhost:8888/api/delete/{id} (GET)
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<Persona> delete(@PathVariable Long id) {
 		Persona persona = personaService.get(id);
@@ -49,7 +51,7 @@ public class PersonaRestController {
 		}
 	}
 	
-	// http://localhost:8888/find/{id} (GET)
+	// http://localhost:8888/api/find/{id} (GET)
 		@RequestMapping(value = "/find/{id}", method = RequestMethod.GET, produces = "application/json")
 		public Persona showSave(@PathVariable("id") Long id) {
 			return personaService.get(id);
